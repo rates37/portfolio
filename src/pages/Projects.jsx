1a4a396ff69aa56e7005814ef85312ad1f95f607
Satya Jhaveri <satyajhaveri@protonmail.ch>
2025-01-04 12:39:00 +1100

    Added description of project page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,7 +12,14 @@ const Projects = () => {
           </span>
         </h1>
         <div className="mt-5 flex flex-col gap-3 text-slate-400">
-          <p>Some description about projects</p>
+          <p>Over the years, I've had the privilege of working on a wide range
+            of projects, each offering its own unique challenges and learning
+            experiences. Among them, these are the ones I'm most passionate
+            about. Many are open-source, and I encourage you to explore the
+            codebase if one of them catches your interest. Contributions and
+            feedback are always welcome, as I believe in the power of 
+            collaboration to drive continuous development.
+          </p>
         </div>
       </section>
     </div>
