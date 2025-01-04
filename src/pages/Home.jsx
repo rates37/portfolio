9ef948397232d0325a3923fff4d21dbef864a6f2
Satya Jhaveri <satyajhaveri@protonmail.ch>
2025-01-04 12:28:46 +1100

    Home page bg colour consistent with rest of application

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -46,7 +46,7 @@ const Home = () => {
 
       {/* 3D Screen */}
       <Canvas
-        className={`w-full h-screen bg-black ${
+        className={`w-full h-screen bg-zinc-900 ${
           isMouseDown ? "cursor-grabbing" : "cursor-grab"
         }`}
         camera={{ near: 0.01, far: 1000 }}
