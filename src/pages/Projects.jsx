98b7e610dc4a0e0729415f61b59d740468e92247
Satya Jhaveri <satyajhaveri@protonmail.ch>
2025-01-04 12:57:30 +1100

    Completed projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { projects } from "../constants";
+import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
 
 const Projects = () => {
   return (
@@ -12,15 +15,53 @@ const Projects = () => {
           </span>
         </h1>
         <div className="mt-5 flex flex-col gap-3 text-slate-400">
-          <p>Over the years, I've had the privilege of working on a wide range
-            of projects, each offering its own unique challenges and learning
+          <p>
+            Over the years, I've had the privilege of working on a wide range of
+            projects, each offering its own unique challenges and learning
             experiences. Among them, these are the ones I'm most passionate
             about. Many are open-source, and I encourage you to explore the
             codebase if one of them catches your interest. Contributions and
-            feedback are always welcome, as I believe in the power of 
-            collaboration to drive continuous development.
+            feedback are always welcome.
           </p>
         </div>
+
+        <div className="flex flex-wrap my-20 gap-16">
+          {projects.map((project) => (
+            <div className="lg:w-[400px] w-full" key={project.name}>
+              <div className="block-container w-12 h-12">
+                <div className={`btn-back rounded-xl ${project.theme}`} />
+                <div className="btn-front rounded-xl flex justify-center items-center">
+                  <img
+                    src={project.iconUrl}
+                    alt="Project Icon"
+                    className="w-1/2 h-1/2 object-contain"
+                  />
+                </div>
+              </div>
+
+              <div className="mt-5 flex flex-col">
+                <h4 className="text-2xl font-poppins font-semibold">
+                  {project.name}
+                </h4>
+                <p className="mt-2 text-slate-400">{project.description}</p>
+                <div className="mt-5 flex items-center gap-2 font-poppins">
+                  <Link
+                    to={project.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="font-semibold text-blue-400 inline-display underline"
+                  >
+                    Link
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <hr className="border-slate-200" />
+
+        <CallToAction />
       </section>
     </div>
   );
