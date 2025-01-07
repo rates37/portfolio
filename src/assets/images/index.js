abe9f6695ec896fc8ba0211c7b86a5cfdf8ca1d9
Satya Jhaveri <satyajhaveri@protonmail.ch>
2025-01-07 14:59:15 +1100

    formatting

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,13 +1,7 @@
-import monash from './monash.svg'
-import mdn from './mdn.svg'
-import byoe from './byoe.svg'
-import viziou from './viziou.png'
-import uniplan from './uniplan.png'
+import monash from "./monash.svg";
+import mdn from "./mdn.svg";
+import byoe from "./byoe.svg";
+import viziou from "./viziou.png";
+import uniplan from "./uniplan.png";
 
-export {
-    monash,
-    mdn,
-    byoe,
-    viziou,
-    uniplan
-}
+export { monash, mdn, byoe, viziou, uniplan };
