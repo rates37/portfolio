8ec510e68ba4d7564877bb2ac46180ab7d9a9ae6
Satya Jhaveri <satyajhaveri@protonmail.ch>
2025-01-12 19:24:37 +1100

    Removed contact route

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
+          {/* <Route path="/contact" element={<Contact />} /> */}
         </Routes>
       </Router>
     </main>
