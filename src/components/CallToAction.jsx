22710917f62f77018b4032d4a798f424441d1941
Satya Jhaveri <satyajhaveri@protonmail.ch>
2025-01-12 19:21:35 +1100

    Added Github link to cta

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -9,11 +9,20 @@ const CallToAction = () => {
         Want to talk? <br className="sm:block hidden" />
         Let's work on something together!
       </p>
-      <Link to={socials.linkedin.link} className="linkedin-btn">
+      <Link to={socials.linkedin.link} className="contact-btn">
         <span className="font-semibold social-link">LinkedIn {" "}<img
                       src={socials.linkedin.iconUrl}
                       alt={socials.linkedin.name}
-                      className="w-1/2 h-1/2 object px-2"
+                      className="w-1/2 h-1/2 object-contain px-2  max-h-6"
+                    /></span>
+      </Link>
+
+      
+      <Link to={socials.github.link} className="contact-btn">
+        <span className="font-semibold social-link">GitHub {" "}<img
+                      src={socials.github.iconUrl}
+                      alt={socials.github.name}
+                      className="w-1/2 h-1/2 object-contain px-2 max-h-6"
                     /></span>
       </Link>
     </section>
